a070d7cb2a4badf3284db706d8ed0c2c3793f381
Lukas Woodtli <woodtli.lukas@gmail.com>
2020-01-04 15:40:38 +0100

    Finish example application

--- todo/app.js
+++ todo/app.js
@@ -1,13 +1,27 @@
 var flatiron = require('flatiron'),
-    path = require('path'),
-    app = flatiron.app;
+  path = require('path'),
+  nstatic = require('node-static'),
+  app = flatiron.app;
 
 app.config.file({ file: path.join(__dirname, 'config', 'config.json') });
 
-app.use(flatiron.plugins.http);
+var file = new nstatic.Server(__dirname + '/public/');
 
-app.router.get('/', function () {
-  this.res.json({ 'hello': 'world' })
-});
+
+app.use(flatiron.plugins.http, {
+  before: [
+    require('flatware-cookie-parser')(),
+    require('flatware-session')(),
+    function (req, res) {
+      var found = app.router.dispatch(req, res); if (!found) {
+        file.serve(req, res);
+      }
+    }]
+}
+);
+
+app.router.path('/users', require('./routes/users'));
+app.router.path('/session', require('./routes/session'));
+app.router.path('/todos', require('./routes/todos'));
 
 app.start(3000);
