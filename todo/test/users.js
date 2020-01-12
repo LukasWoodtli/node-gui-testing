882af904818b1315232697fd9877bc5dde70fe23
Lukas Woodtli <woodtli.lukas@gmail.com>
2020-01-12 16:39:26 +0100

    Add test that fills login form

--- todo/test/users.js
+++ todo/test/users.js
@@ -5,19 +5,33 @@
 var assert = require("assert");
 var Browser = require("zombie");
 var app = require("../app");
+var fixtures = require("./fixtures");
+var couchdb = require('../lib/couchdb'),
+    dbName = 'users',
+    db = couchdb.use(dbName);
 
-before(function (done) {
-    app.start(3000, done);
-});
-
-after(function (done) {
-    app.server.close(done);
-    // see top of file:
-    // wtf.dump();
-});
 
 describe("Users", function () {
+    before(function (done) {
+        app.start(3000, done);
+    });
+    
+    after(function (done) {
+        app.server.close(done);
+        // see top of file:
+        // wtf.dump();
+    });
+
     describe("Signup Form", function () {
+
+        before(function(done) {
+            db.get(fixtures.user.email, function(err, doc) {
+                if (err && err.status_code === 404) return done();
+                if (err) throw err;
+                db.destroy(doc._id, doc._rev, done);
+            });
+        });
+
         it("should load the signup form", function (done) {
             var options = {
                 headers: {
@@ -52,6 +66,30 @@ describe("Users", function () {
                 done();
             });
         });
-    });
+
+        it("should submit", function (done) {
+            Browser.visit("http://localhost:3000/users/new", function (err, browser) {
+                if (err) throw err;
+
+                /* It would be possible to use `browser.fill('#email', 'me@email.com')` and 
+                ** `browser.fill('#password', 'mypassword')`.
+                ** But the labels for each text field are using a `for` attribute that
+                ** indicates the `id` attribute of the tag it belongs to.
+                */
+                browser.fill('E-mail', fixtures.user.email);
+                browser.fill('Password', fixtures.user.password);
+
+
+                browser.pressButton('Submit', function (err) {
+                    if (err) throw err;
+                    assert.equal(browser.location.pathname, '/users');
+                    assert.equal(browser.text('h1'), 'Thank you!');
+                    assert(browser.query('a[href="/session/new"]'), 'has login link');
+                    done();
+                });
+            }); 
+        });
+    })
 });
+
 
