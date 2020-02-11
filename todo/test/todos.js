3d8cb01cf64069b17576df047aa4f67be830ace7
Lukas Woodtli <woodtli.lukas@gmail.com>
2020-02-11 21:12:10 +0100

    Log error for travis

--- todo/test/todos.js
+++ todo/test/todos.js
@@ -79,8 +79,8 @@ describe('Todos', function () {
 
         it('should allow to create a todo', login(function (browser, done) {
             browser.visit("http://localhost:3000/todos/new", function (err) {
-                if (err) throw err;
-                console.log(err);
+            console.log(err);    
+            if (err) throw err;
 
                 browser.fill('what', 'Laundry');
                 browser.pressButton('Create', function (err) {
